afaa15f7de36ee206cff5c141ad372639bfa7de4
Yaroslav Halchenko <debian@onerussian.com>
2025-01-27 16:49:42 -0500

    My attempt to recreate venn diagram from the past but with AI added

--- posters/incf2019-repronim-containers.R
+++ posters/incf2019-repronim-containers.R
@@ -1,4 +1,4 @@
 #!/usr/bin/env Rscript
 
 library('webshot')
-webshot("https://github.com/ReproNim/containers/blob/master/README.md", vwidth=1400, "incf2019-repronim-containers.pdf")
+webshot("https://github.com/ReproNim/containers/blob/master/README.md", vwidth=1400, "incf2019-repronim-containers_.pdf")
